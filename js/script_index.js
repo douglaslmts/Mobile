1ef57a877f63420d6f6cc68788a75bb73b496839
douglaslmts <douglongaray@gmail.com>
2021-01-05 14:06:30 -0300

    div se auto ajustando

--- js/script_index.js
+++ js/script_index.js
@@ -1,1 +1,14 @@
-window.addEventListener('resize',function(){window.alert('mexeu na tela')})
+var x = document.getElementById("divbody")
+var
+
+    window.onload = function() {
+    redimenciona()
+};
+
+window.addEventListener('resize', function() { redimenciona() })
+
+function redimenciona() {
+    x.style.backgroundColor = "blueviolet";
+    x.style.width = window.innerWidth - 0.5 + "px";
+    x.style.height = window.innerHeight + "px";
+}
