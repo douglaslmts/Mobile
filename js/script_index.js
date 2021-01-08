5b530889eb64996826470a859b704771dd4a37da
douglaslmts <douglongaray@gmail.com>
2021-01-08 14:43:48 -0300

    ajustando div

--- js/script_index.js
+++ js/script_index.js
@@ -1,14 +1,19 @@
 var x = document.getElementById("divbody")
-var
 
-    window.onload = function() {
+window.onload = function() {
     redimenciona()
 };
 
 window.addEventListener('resize', function() { redimenciona() })
 
 function redimenciona() {
-    x.style.backgroundColor = "blueviolet";
-    x.style.width = window.innerWidth - 0.5 + "px";
-    x.style.height = window.innerHeight + "px";
+    x.style.margin = "0px auto";
+    x.style.textAlign = "center";
+    x.style.backgroundColor = "white";
+    x.style.border = "solid";
+    x.style.width = window.innerWidth - 100 + "px";
+    x.style.height = window.innerHeight - 20 + "px";
+    x.style.color = "whitblacke";
+    x.style.fontFamily = "Arial";
+    x.style.fontSize = "12px";
 }
