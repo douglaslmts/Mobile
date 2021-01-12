f03a2fef1fe88c79684f996793016c17fe8e393c
douglaslmts <douglongaray@gmail.com>
2021-01-12 17:59:35 -0300

    imitando nubank

--- js/script_index.js
+++ js/script_index.js
@@ -1,5 +1,8 @@
 var x = document.getElementById("div1")
 var a = document.getElementById("div2")
+var b = document.getElementById("div3")
+var c = document.getElementById("div4")
+
 
 window.onload = function() {
     redimenciona()
@@ -8,20 +11,44 @@ window.onload = function() {
 window.addEventListener('resize', function() { redimenciona() })
 
 function redimenciona() {
-    x.style.margin = "0px auto";
-    x.style.backgroundColor = "";
+    x.style.margin = "150px auto";
+    x.style.backgroundColor = "#9900cc";
     x.style.border = "";
-    x.style.width = window.innerWidth - 10 + "px";
-    x.style.height = window.innerHeight - 10 + "px";
-    x.style.color = "whitblacke";
-    x.style.fontFamily = "Arial";
-    x.style.fontSize = "12px";
+    x.style.width = window.innerWidth + "px";
+    x.style.height = window.innerHeight + "px";
+    x.style.color = "white";
+    x.style.fontFamily = "font-family: Graphik-Medium, Graphik-Regular, 'Gotham SSm A', 'Gotham SSm B', 'Helvetica Neue', Helvetica, Arial, sans-serif";
+    x.style.fontSize = "300%";
+    x.style.fontWeight = "bold";
+    x.style.height = window.innerHeight - 300 + "px";
+    x.style.width = window.innerWidth - 100 + "px";
 
-    a.style.backgroundColor = "#9C32DB";
-    a.style.textAlign = "center";
+    a.style.backgroundColor = "white";
+    a.style.marginTop = "50px";
+    a.style.border = "";
+    a.style.textAlign = "left";
     a.style.width = "100%";
-    a.style.height = "300px";
-    a.style.fontSize = "200px";
-    a.style.color = "white";
+    a.style.height = "400px";
+    a.style.borderRadius = "10px";
 
+    b.style.backgroundColor = "white";
+    b.style.textAlign = "left";
+    b.style.border = "";
+    b.style.fontFamily = "font-family: Graphik-Medium, Graphik-Regular, 'Gotham SSm A', 'Gotham SSm B', 'Helvetica Neue', Helvetica, Arial, sans-serif";
+    b.style.width = "100%";
+    b.style.marginTop = "50px";
+    b.style.height = "400px";
+    b.style.fontSize = "200px";
+    b.style.color = "white";
+    b.style.borderRadius = "10px";
+
+    c.style.backgroundColor = "white";
+    c.style.border = "";
+    c.style.textAlign = "center";
+    c.style.width = "100%";
+    c.style.marginTop = "50px";
+    c.style.height = "400px";
+    c.style.fontSize = "200px";
+    c.style.color = "white";
+    c.style.borderRadius = "10px";
 }
